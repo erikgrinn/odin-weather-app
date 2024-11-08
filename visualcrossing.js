bd308adc4788bf785fb4001d1467edb527f7aa59
Erik Grinn <grinn.erik@gmail.com>
2024-11-08 18:29:12 -0500

    gets data

---
+++ visualcrossing.js
@@ -0,0 +1,20 @@
+const dataDiv = document.querySelector('#data')
+
+// URL (required), options (optional)
+fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ridgewood?key=86DXP8CF2L7LZV4FNKA8SH9RQ', {mode: 'cors'})
+  .then(function(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok: ${response.status} $response.statusText}')
+    }
+    // Successful response :)
+    return response.json()
+  })
+  .then(function(data) {
+    // console.log(data)
+    dataDiv.textContent = JSON.stringify(data, null, 2)
+  })
+  .catch(function(err) {
+    // Error :(
+    console.log('err')
+  });
+
