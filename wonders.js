4957993f30be699449b4b08207e5a6e9d502aef8
Erik Grinn <grinn.erik@gmail.com>
2024-11-08 19:50:30 -0500

    wonders

--- wonders.js
+++ wonders.js
@@ -1,21 +1,22 @@
+
 const dataDiv = document.querySelector('#data')
+const imgDiv = document.querySelector('img')
+
 
 // URL (required), options (optional)
-fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ridgewood?key=86DXP8CF2L7LZV4FNKA8SH9RQ', {mode: 'cors'})
+fetch('https://www.world-wonders-api.org/v0/wonders/', {mode: 'cors'})
   .then(function(response) {
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
     // Successful response :)
-    console.log(response)
     return response.json()
   })
   .then(function(data) {
-    // console.log(data)
-    dataDiv.textContent = JSON.stringify(data, null, 2)
+    dataDiv.textContent = data[0].name
+    imgDiv.setAttribute('src', data[0].links.images[0])
   })
   .catch(function(err) {
     // Error :(
     console.log('err')
   });
-
